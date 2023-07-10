5385bf6e9d7f04776d4d1aaf1386b8bc42c93150
Gregorio Caba <100645620+grichal@users.noreply.github.com>
2023-07-09 22:53:45 -0400

    se arreglaron los enrutadores

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter,Routes,Route,} from "react-router-dom";
+import { BrowserRouter,Routes,Route,HashRouter} from "react-router-dom";
 import Inventario from "./components/inventario/Inventario";
 import Mantenimiento from "./components/mantenimiento/Mantenimiento";
 import Navi from "./components/nav/Nav";
@@ -12,13 +12,13 @@ function App() {
   return (
     <div>
       <Navi/>
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
         <Route path="/" element={<Inventario/>} exact></Route>
         <Route path="/mantenimiento" element={<Mantenimiento/>} exact></Route>
         <Route path="/test" element={<Carousel/>} exact/>
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <ToastContainer/>
       </div>
   )
